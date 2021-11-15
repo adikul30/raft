e6a90687627da048721100589b4d4543f45da005
adikul30 <adi.kul30@gmail.com>
2021-11-14 21:15:34 -0500

    create rpc clients

--- raft/raft.go
+++ raft/raft.go
@@ -293,8 +293,19 @@ func (r *Raft) RequestVote(args RequestVoteArgs, reply *RequestVoteReply) error 
 }
 
 func (r *Raft) requestVoteRPC(peerId int, args RequestVoteArgs, reply *RequestVoteReply) error {
+	if r.peers[peerId].Client == nil {
+		r.constructClientEndpoint(peerId)
+	}
 	err := r.peers[peerId].Client.Call("Raft.RequestVote", args, reply)
 	return err
+}
+
+func (r *Raft) constructClientEndpoint(peerId int) {
+	client, err := rpc.DialHTTP("tcp", "127.0.0.1:" + r.peers[peerId].Port)
+	if err != nil {
+		log.Fatal("dialing:", err)
+	}
+	r.peers[peerId].Client = client
 }
 
 func (r *Raft) appendEntriesRPC(peerId int, args AppendEntriesArgs, reply *AppendEntriesReply) error {
